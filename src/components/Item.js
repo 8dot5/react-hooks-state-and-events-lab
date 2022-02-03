ce94fe86ea93d2ce5c3c5a435230a38d73293096
Chun Yi <8.5by11@gmail.com>
2022-02-03 08:31:24 -0500

    commit tree b02595ef5d698ec79cacfdc6ef9173c9a22e5d98

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ addCart , setAddCart ] = useState(false); //initial render of false as in 0
+ 
+  function handleClick() {
+    setAddCart((addCart) => !addCart)
+  }
+
   return (
-    <li className="">
+    <li className={addCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{addCart ? "Add to Cart" : "Remove from Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,41 @@
-import React from "react";
+//https://learning.flatironschool.com/courses/4986/assignments/165749?module_item_id=357569
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [ selectedCategory, setSelectedCategory ] = useState("All")
+
+  function handleFilterSelect(e) {
+    setSelectedCategory(e.target.value)
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  })
+
+  const itemsToDisplay2 = items.filter(item => item.category === selectedCategory)
+
+  //ternary
+  // const itemsToDisplay = items.filter((item) => {
+  //   return ( selectedCategory === "All" ? true : item.category.includes(selectedCategory) )
+  // })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={handleFilterSelect}>
+          <option value="All" >Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay2.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
